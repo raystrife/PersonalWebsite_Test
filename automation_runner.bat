b5aff4d1b2ff22c307309e69e0934e22504277b1
raystrife <33677820+raystrife@users.noreply.github.com>
2020-07-20 11:15:16 -0400

    Committed the batch file to run the test

---
+++ automation_runner.bat
@@ -0,0 +1,4 @@
+py.test -s -v tests/navigation/navigation_test.py --browser chrome
+py.test -s -v tests/project/project_test.py --browser chrome
+py.test -s -v tests/interest/interest_test.py --browser chrome
+py.test -s -v tests/contact/contact_test.py --browser chrome
